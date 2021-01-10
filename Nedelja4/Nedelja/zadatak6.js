5c6f60f0b260e91d032cb47331f4cc6a7145b892
Jelena Nikolic <jelena.h.nikolic@gmail.com>
2021-01-10 12:38:32 +0100

    Ispravljen domaci

--- Nedelja4/Nedelja/zadatak6.js
+++ Nedelja4/Nedelja/zadatak6.js
@@ -4,26 +4,28 @@
 //ii. Дугмићи за 'наручивање одмах' ресетују бројач на нула и исписују додатну поруку (Порука у облику: Наручили сте [X] пица, [ danasnji datum i vreme ])
 //  Напомена: X је бројач, проверити да ли је бројач 0, и ако јесте кликом на наручивање одмах избацује одговарајућу поруку за грешку
 
-let count = 1
-let time = new Date()
+let count = 0
 
-const spanValue = document.querySelector(`#value`)
-const btns = document.querySelectorAll(`.btn`)
+const btnNaruci1 = document.querySelector(`.btn order1`)
+const btnNaruci2 = document.querySelector(`.btn order2`)
+const btnReset3 = document.querySelector(`.btn reset3`)
+const btnReset4 = document.querySelector(`.btn reset4`)
 
-btns.forEach(function (btn) {
-  btn.addEventListener(`click`, function (e) {
+btnNaruci1.addEventListener(`click`, naruci)
+btnNaruci2.addEventListener(`click`, naruci)
+btnReset3.addEventListener(`click`, naruciOdmah)
+btnReset4.addEventListener(`click`, naruciOdmah)
 
-    spanValue.textContent = count
-    const dugme = e.currentTarget.classList
+const naruci = () => {
+  count++
+  console.log(count)
+}
+const naruciOdmah = () => {
+  if(count == 0){
+    console.log(`GRESKA`)
+  }else{
+    console.log(`Narucili ste ${count} pica`)
+    count = 0
+  }
+}
 
-    if (dugme.contains(`order1`)){
-      count--
-    } else if (dugme.contains(`order2`)){
-      count++
-    } else if(dugme.contains(`reset`)){
-      console.log(`Narucili ste ${count} pica, ${new Date()} `)
-    }
-  })
-  
-
-})
