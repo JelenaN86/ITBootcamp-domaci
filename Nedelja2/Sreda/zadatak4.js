b4062d86474c4e901893a6a112b7dfe2f1c7d455
Jelena Nikolic <jelena.h.nikolic@gmail.com>
2021-01-10 12:48:55 +0100

    Ispravljen domaci

--- Nedelja2/Sreda/zadatak4.js
+++ Nedelja2/Sreda/zadatak4.js
@@ -1,9 +1,14 @@
 //Zadatak 4.
 //Исписати првих 1000 бројева дељивих са 5
 
-
-for (let num = 0; num <= 1000; num++){
-    if(num % 5 == 0){
-        console.log(num)
-    }
+let n = 0
+    
+for(i = 1;; i++){
+   if(i % 5 == 0){ 
+   console.log(i)
+    n++
+   }
+   if(n == 1000){
+        break
+   }
 }
